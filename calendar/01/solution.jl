9ff157d970a3484b6701422f17d532aba5f44154
Jack Shannon <j.b.shannon@lse.ac.uk>
2021-12-01 13:02:00 +0000

    solved day 1

---
+++ calendar/01/solution.jl
@@ -0,0 +1,10 @@
+# Load data
+data = parse.(Int, readlines(joinpath(@__DIR__, "input.txt")))
+
+# Part 1
+ans1 = sum([data[i] > data[i-1] for i in 2:length(data)])
+@info "Number of times depth measurements increases = $ans1"
+
+# Part 2
+ans2 = sum([sum(data[i-2:i]) > sum(data[i-3:i-1]) for i in 4:length(data)])
+@info "Number of times the sum of a three-measurement window increases = $ans2"
